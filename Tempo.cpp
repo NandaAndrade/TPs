f27f83bfa9d8228de09e76355fc046b3e80aa374
Nanda Andrade <48488203+NandaAndrade@users.noreply.github.com>
2021-01-31 12:17:57 -0300

    set constructor 0

--- Tempo.cpp
+++ Tempo.cpp
@@ -2,62 +2,81 @@
 #include "tempo.h"
 #include <iostream>
 
-using namespace std;    
+using namespace std;
 
 // contrutor parametrico 1 -> cria um tempo com dias, horas, min e seg definidos.
-Tempo::Tempo(int dias, int horas, int min, int seg){
+Tempo::Tempo(int dias, int horas, int min, int seg)
+{
+    _dias = 0;
+    _horas = 0;
+    _min = 0;
+    _seg = 0;
+
     _dias = dias;
     _horas = horas;
     _min = min;
     _seg = seg;
 
-this->checkConsistence(_dias, _horas, _min, _seg);
+    this->checkConsistence(_dias, _horas, _min, _seg);
 }
 
 // getters
-int Tempo::getDias(){
+int Tempo::getDias()
+{
     return _dias;
 }
 
-int Tempo::getHoras(){
+int Tempo::getHoras()
+{
     return _horas;
 }
 
-int Tempo::getMin(){
+int Tempo::getMin()
+{
     return _min;
 }
 
-int Tempo::getSeg(){
+int Tempo::getSeg()
+{
     return _seg;
 }
 
 // Checa a consistencia do tempo e altera caso esteja errado.
-void Tempo::checkConsistence(int dias, int horas, int min, int seg){
+void Tempo::checkConsistence(int dias, int horas, int min, int seg)
+{
     int horasAcum = horas;
     int minAcum = min;
 
-    if(seg > 59){
+    if (seg > 59)
+    {
         _seg = seg % 60;
         minAcum += (seg / 60);
     }
 
-    if(minAcum > 59){
+    if (minAcum > 59)
+    {
         _min = minAcum % 60;
         horasAcum += (minAcum / 60);
-    } else{
+    }
+    else
+    {
         _min = minAcum;
     }
 
-    if(horasAcum > 23){
+    if (horasAcum > 23)
+    {
         _horas = horasAcum % 24;
         _dias += (horasAcum / 24);
-    } else{
+    }
+    else
+    {
         _horas = horasAcum;
     }
 }
 
 // Soma um tempo com outro tempo, serve para atualizar.
-Tempo Tempo::sumTempo(Tempo t){
+Tempo Tempo::sumTempo(Tempo t)
+{
     int totalDias = t.getDias() + _dias;
     int totalHoras = t.getHoras() + _horas;
     int totalMin = t.getMin() + _min;
@@ -67,7 +86,8 @@ Tempo Tempo::sumTempo(Tempo t){
 }
 
 // Imprimir o tempo
-void Tempo::print(){
+void Tempo::print()
+{
     cout << "Time:" << endl;
     cout << "Total Dias: " << _dias << endl;
     cout << "Total Horas: " << _horas << endl;
@@ -76,7 +96,8 @@ void Tempo::print(){
 }
 
 // Inicializa o construtor pedindo o usuário as entradas
-Tempo Tempo::enterData(){        
+Tempo Tempo::enterData()
+{
     int dias = 0;
     int horas = 0;
     int min = 0;
@@ -97,19 +118,19 @@ Tempo Tempo::enterData(){
     return Tempo(dias, horas, min, seg);
 }
 
-void Tempo::addSeg(int seg){
+void Tempo::addSeg(int seg)
+{
 
     _seg += seg;
 
     this->checkConsistence(_dias, _horas, _min, _seg);
 }
 
-void Tempo::decSeg(int seg){
+void Tempo::decSeg(int seg)
+{
 
-    if(_seg >= seg)
+    if (_seg >= seg)
         _seg -= seg;
 
     this->checkConsistence(_dias, _horas, _min, _seg);
 }
-
-
