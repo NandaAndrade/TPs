4f482868d5b0e258739a6f43abd2de68947a41e8
Nanda Andrade <fernandaandradebc@hotmail.com>
2021-02-06 16:56:43 -0300

    parte final com erros de sobrecarga

--- TP3/src/Ponto2D.h
+++ TP3/src/Ponto2D.h
@@ -1,27 +1,29 @@
+#include <iostream>
+#include <map>
+class Ponto2D;
 
-
+std::map<int, Ponto2D *> pontos;
 class Ponto2D
 {
 private:
     double x;
     double y;
-    int id = 0; //Usar Map
+    int id;
     int getNextid();
 
 public:
     Ponto2D();
     Ponto2D(double a, double b = 0);
     ~Ponto2D(); //decrementar o id
-    double getX () const;
-    double getY () const;
-    void setX (double x); //inline não funciona
-    void setY (double y);
+    double getX() const;
+    double getY() const;
+    void setX(double x); //inline não funciona
+    void setY(double y);
     void print() const;
     double distToOrig();
-    double distTo(const Ponto2D& p2) const;
-    Ponto2D sumOf(const Ponto2D& p2);
+    double distTo(const Ponto2D &p2) const;
+    void virtual sumOf(const Ponto2D &p2);
+    Ponto2D sumOf(const Ponto2D &p2);
     int getid() const;
-   // Ponto2D sumPoint(Ponto2D);
+    // Ponto2D sumPoint(Ponto2D);
 };
-
-
