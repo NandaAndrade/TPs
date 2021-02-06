3fde22a99596a5222d95e8466161c20eb6b4d13c
Nanda Andrade <fernandaandradebc@hotmail.com>
2021-02-06 18:14:44 -0300

    fase de teste

--- TP3/src/Ponto2D.cpp
+++ TP3/src/Ponto2D.cpp
@@ -2,83 +2,65 @@
 #include <cmath>
 #include "Ponto2D.h"
 
-Ponto2D ::Ponto2D()
-{
+Ponto2D :: Ponto2D(){
     x = 0;
     y = 0;
-    id = getNextid();
 }
 
-Ponto2D ::Ponto2D(double xx, double yy)
-{
+Ponto2D :: Ponto2D(double xx, double yy){
     x = xx;
     y = yy;
-    id = getNextid();
 }
 
-double Ponto2D ::getX() const
-{
+double Ponto2D :: getX() const{
     return x;
 }
 
-double Ponto2D ::getY() const
-{
+double Ponto2D :: getY() const{
     return y;
 }
 
-int Ponto2D ::getNextid()
-{
-    int id = pontos.size() + 1;
-    pontos.insert(std::pair<int, Ponto2D *>(id, this));
+int Ponto2D :: getNextid(){
+    //Trocar por Map
     return id;
 }
 
-Ponto2D ::~Ponto2D()
-{
-    std::map<int, Ponto2D *>::iterator it;
-    it = pontos.find(id);
-    pontos.erase(it);
+Ponto2D :: ~Ponto2D(){
+    //Algo relacionado com Map.
 }
 
-double Ponto2D ::distToOrig()
-{
-    return sqrt(x * x + y * y);
+double Ponto2D :: distToOrig(){
+    return sqrt(x*x + y*y);
 }
 
-double Ponto2D ::distTo(const Ponto2D &p2) const
-{
+double Ponto2D :: distTo(const Ponto2D& p2) const{
     double x1 = x - p2.getX();
     double y1 = y - p2.getY();
-    return sqrt(x1 * x1 + y1 * y1);
+    return sqrt (x1*x1 + y1*y1);
 }
 
-Ponto2D Ponto2D ::sumOf(const Ponto2D &p2)
-{
+Ponto2D Ponto2D :: sumOf(const Ponto2D& p2){
     double x1 = x + p2.getX();
     double y1 = y + p2.getY();
-    return Ponto2D(x1, y1);
+    return Ponto2D (x1,y1);
 }
 
-Ponto2D sumPoint(const Ponto2D &p2)
-{
+//Ponto2D sumPoint(const Ponto2D& p2){
+
+//}
+
+inline int Ponto2D :: getid() const{
+    return this-> id;
 }
 
-inline int Ponto2D ::getid() const
-{
-    return this->id;
-}
-
-void Ponto2D ::setX(double x_)
-{
+void Ponto2D :: setX(double x_){
     x = x_;
 }
 
-void Ponto2D ::setY(double y_)
-{
+void Ponto2D :: setY(double y_){
     y = y_;
 }
 
-void Ponto2D ::print() const
-{
+void Ponto2D :: print() const{
     std::cout << "(" << x << "," << y << ")" << std::endl;
 }
