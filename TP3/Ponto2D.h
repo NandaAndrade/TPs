15811479856119971c845fe0190756c5cc0edaed
Nanda Andrade <48488203+NandaAndrade@users.noreply.github.com>
2021-02-06 08:55:58 -0300

    Funçoes membro no .h

--- TP3/Ponto2D.h
+++ TP3/Ponto2D.h
@@ -8,6 +8,23 @@ public:
     Ponto2D();
     Ponto2D(float x, float y);
 
+    // Função membro para escrever (imprimir) as coordenadas do ponto na tela
+    void print();
+
+    // Função membro para calcular a distância do ponto a origem do sistema de coordenadas cartesiano
+    double distToOrig() const;
+
+    // Função membro para calcular a distância entre dois pontos p1 e p2, onde p1 é o objeto que chama a função
+    double distTo(Ponto2D p) const;
+
+    // Função membro que modifica as coordenadas do objeto corrente p1 somando às
+    // suas coordenadas um novo ponto p2, que deve ser passado por argumento.
+    void sumOf(Ponto2D p);
+
+    // Função membro que retorna um ponto p3 que é o resultado da soma entre dois pontos p1 e p2,
+    // onde p1 é o objeto que chama a função.
+    Ponto2D sumOf(Ponto2D p) override;
+
     //o destrutor tem que decrementar id disponivel
     ~Ponto2D();
 
@@ -15,4 +32,4 @@ private:
     float _x;
     float _y;
     int _id;
-}
+};
